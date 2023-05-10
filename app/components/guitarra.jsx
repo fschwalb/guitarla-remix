0002920ea837bf17aa7843b9416ce310dbecb768
Fernando Schwalb Costa <fschwalb@gmail.com>
2023-05-10 19:03:02 +0200

    show img of products

--- app/components/guitarra.jsx
+++ app/components/guitarra.jsx
@@ -1,9 +1,20 @@
+import { Link } from "@remix-run/react";
+
 
 
 export default function Guitarra({ guitarra }) {
 
-    console.log(guitarra);
+    const { descripcion, imagen, precio, url, nombre } = guitarra;
     return (
-        <div>guitarra</div>
+        <div className="guitarra">
+            <div className="contenido">
+                <img src={imagen.data.attributes.formats.medium.url} alt={`Imagen guitarra ${nombre}`} />
+                <h3>{nombre}</h3>
+                <p className="descripcion">{descripcion}</p>
+                <p className="precio">{precio}</p>
+
+                <Link className="enlace" to={`/guitarras/${url}`}>Ver Producto</Link>
+            </div>
+        </div>
     )
 }
