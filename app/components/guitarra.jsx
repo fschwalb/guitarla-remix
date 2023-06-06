bca8a62306f96340917e57a62a9a8440e36359ff
Fernando Schwalb Costa <fschwalb@gmail.com>
2023-06-06 11:41:54 +0200

    again error getting NULL

--- app/components/guitarra.jsx
+++ app/components/guitarra.jsx
@@ -21,3 +21,5 @@ export default function Guitarra({ guitarra }) {
         </div>
     )
 }
+
+
--- app/routes/guitarras.$guitarraUrl.jsx
+++ app/routes/guitarras.$guitarraUrl.jsx
@@ -1,4 +1,15 @@
+import { getGuitarra } from '~/models/guitarras.server';
 
+export async function loader ({ params }) {
+
+  const { guitarraUrl } = params;
+
+  const guitarra = await getGuitarra( guitarraUrl )
+  
+  console.log(guitarra);
+
+  return {}
+}
 
 function Guitarra() {
   return (
